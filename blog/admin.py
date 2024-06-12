8e4ead78cf200796b68cdbc5f2016f26e50fb4cc
parsa <paghsis@gmail.com>
2024-06-12 19:44:32 +0330

    improve columns of post table

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import  Post
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'date_modified')
+
+
 # Register your models here.
-admin.site.register(Post)
+# admin.site.register(Post, PostAdmin)
