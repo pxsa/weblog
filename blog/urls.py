2b87a7da57c5f4404a8914866fff9dc294385e7d
parsa <paghsis@gmail.com>
2024-06-13 19:00:21 +0330

    templates

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('hi/', views.say_hi, name='say_hi'),
+    path('', views.all, name='all_posts')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 # Create your views here.
 def say_hi(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def all(request):
+    all_posts = Post.objects.all()
+    return render(request, 'blog/posts.html', {'posts': all_posts})
