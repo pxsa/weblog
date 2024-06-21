db154b9681e537c521c50ed2232b6b3e3ab6299a
parsa <paghsis@gmail.com>
2024-06-21 12:01:51 +0330

    class view

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hi/', views.say_hi, name='say_hi'),
-    path('', views.all, name='all_posts'), 
-    path('detail/<int:post_id>', views.detail, name='post_detail'),
+    # path('', views.all, name='all_posts'), 
+    path('', views.PostList.as_view(), name='all_posts'), 
+
+    # path('detail/<int:pk>', views.detail, name='post_detail'),
+    path('detail/<int:pk>', views.PostDetail.as_view(), name='post_detail'),
 
     path('create/', views.create_post, name='create_post'),
-    path('update/<int:post_id>', views.update_post, name='update_post'),
-    path('delete/<int:post_id>', views.remove_post, name='remove_post'),
+    path('update/<int:pk>', views.update_post, name='update_post'),
+    path('delete/<int:pk>', views.remove_post, name='remove_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,34 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
-from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.views import generic
 
 from .models import Post
 from .forms import PostCreateForm
 
-# Create your views here.
-def say_hi(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
 
-def all(request):
-    all_posts = Post.objects.filter(status='pub')
-    return render(request, 'blog/posts.html', {'posts': all_posts})
+# def all(request):
+#     all_posts = Post.objects.filter(status='pub')
+#     return render(request, 'blog/posts.html', {'posts': all_posts})
 
-def detail(request, post_id):
-    # post = Post.objects.get(id = post_id)
-    post = get_object_or_404(Post, pk=post_id)
-    if post is None:
-        return HttpResponse("page not founded", status=401 )
-    return render(request, 'blog/detail.html', {'post': post})
+class PostList(generic.ListView):
+    model = Post
+    template_name = 'blog/posts.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(status='pub').order_by('-date_modified')
+
+# def detail(request, post_id):
+#     # post = Post.objects.get(id = post_id)
+#     post = get_object_or_404(Post, pk=post_id)
+#     if post is None:
+#         return HttpResponse("page not founded", status=401 )
+#     return render(request, 'blog/detail.html', {'post': post})
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
 
 
 def create_post(request):
@@ -48,6 +58,8 @@ def update_post(request, post_id):
     return render(request, 'blog/update_post.html', context=context)
 
 def remove_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+
     Post.objects.filter(id=post_id).delete()
     return redirect('all_posts')
 
