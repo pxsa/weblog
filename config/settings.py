8548147d09f2259f544d943804429dea463cf47b
parsa <paghsis@gmail.com>
2024-09-10 17:19:12 +0330

    using mysql configuration

--- config/settings.py
+++ config/settings.py
@@ -76,12 +76,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'weblog',
+        'USER': 'root',
+        'PASSWORD': 'Parsamysql1379',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 
 
 # Password validation
